f0c97ac206a65ebeee5ae1a2b1d859a0ef66ce72
shibamoto <silverbirder@gmail.com>
2021-11-22 11:28:49 +0900

    🙂 Upload tiktok video

--- kubernetes/resources/cronjob/transfer/app.py
+++ kubernetes/resources/cronjob/transfer/app.py
@@ -5,8 +5,10 @@ import json
 
 from TikTokApi import TikTokApi, tiktok
 from dotenv import load_dotenv
+from google.cloud import storage
 
 load_dotenv()
+
 tiktok_json = json.loads(os.environ.get('INPUT_DATA', '{}'))
 
 # Decide on a video file name.
@@ -25,13 +27,18 @@ api = TikTokApi.get_instance()
 device_id = api.generate_device_id()
 
 video_bytes = api.get_video_by_download_url(
-    download_url=os.environ.get('DOWNLOAD_URL', ''),
+    download_url=download_url,
     custom_device_id=device_id,
     custom_verifyFp=os.environ.get('CUSTOM_VERIFY_FP', '')
 )
 
 with open(file_name, "wb") as out:
     out.write(video_bytes)
-    tiktok_json['video']['downloadAddr'] = 'https://storage.googleapis.com/xxx/{file_name}'.format(file_name=file_name)
+    client = storage.Client()
+    bucket_name = '{gcp_project}-bucket'.format(gcp_project=os.environ.get('GCP_PROJECT', ''))
+    bucket = client.get_bucket(bucket_name)
+    new_blob = bucket.blob(file_name)
+    new_blob.upload_from_filename(filename=file_name)
+    tiktok_json['video']['downloadAddr'] = 'https://storage.googleapis.com/{bucket_name}/{file_name}'.format(bucket_name=bucket_name, file_name=file_name)
     print(tiktok_json)
     api.clean_up()
