73f988eb802b06b1ed3deaaa982b882c77fecf29
shibamoto <silverbirder@gmail.com>
2021-11-22 11:18:41 +0900

    🙂 Input json data

--- kubernetes/resources/cronjob/transfer/app.py
+++ kubernetes/resources/cronjob/transfer/app.py
@@ -1,14 +1,16 @@
 import os
 import json
 from urllib.parse import urlparse
+import json
 
-from TikTokApi import TikTokApi
+from TikTokApi import TikTokApi, tiktok
 from dotenv import load_dotenv
 
 load_dotenv()
+tiktok_json = json.loads(os.environ.get('INPUT_DATA', '{}'))
 
 # Decide on a video file name.
-download_url = os.environ.get('DOWNLOAD_URL', '')
+download_url = tiktok_json.get('video', {}).get('downloadAddr', '')
 file_name_candidates = urlparse(download_url).path.split('/')
 candidates_index = len(file_name_candidates)-1
 file_name = 'temporary.mp4'
@@ -18,7 +20,6 @@ while True:
         file_name = '{name}.mp4'.format(name=file_name_candidate)
         break
     candidates_index-=1
-
 
 api = TikTokApi.get_instance()
 device_id = api.generate_device_id()
@@ -31,4 +32,6 @@ video_bytes = api.get_video_by_download_url(
 
 with open(file_name, "wb") as out:
     out.write(video_bytes)
+    tiktok_json['video']['downloadAddr'] = 'https://storage.googleapis.com/xxx/{file_name}'.format(file_name=file_name)
+    print(tiktok_json)
     api.clean_up()
